b334147a54e5dd8d76f15256c2febbb4d82fddff
Chrort <christophero@gmx.de>
2026-04-10 22:34:18 +0200

    fixed issue

--- logout-btn.js
+++ logout-btn.js
@@ -1,17 +1,18 @@
-const logoutHref = document.getElementById("carousel-item-main").querySelectorAll("a")[document.getElementById("carousel-item-main").querySelectorAll("a").length - 1].href;
-const moreMenuUl = document.querySelector('nav.moremenu.navigation > ul.nav.more-nav.navbar-nav')
-
 window.addEventListener("load", () => {   
-    moreMenuUl.insertBefore(createNewLiElement(), moreMenuUl.firstChild);
+    const aTags = document.getElementById("carousel-item-main")?.querySelectorAll("a");
+    const logoutHref = aTags[aTags.length - 1]?.href;
+    const moreMenuUl = document.querySelector('nav.moremenu.navigation > ul.nav.more-nav.navbar-nav');
+
+    moreMenuUl?.insertBefore(createNewLiElement(logoutHref), moreMenuUl.firstChild);
 });
 
-const createNewLiElement = () => {
+const createNewLiElement = link => {
     let logoutLi = document.createElement("li");
 
     logoutLi.classList.add("nav-item");
     logoutLi.role = "none";
     logoutLi.innerHTML = `<a role="menuitem" class="nav-link"
-                                    href="${logoutHref}"
+                                    href="${link}"
                                     data-disableactive="true"
                                     tabindex="-1"
                                 >

95afbced5a162ce552916039192d85e3961b6976
Chrort <christophero@gmx.de>
2026-04-12 18:02:29 +0200

    dark-theme step 1

---
+++ dark-theme.js
@@ -0,0 +1,4 @@
+let logoimg = document.getElementById("logoimage");
+
+if(!logoimg) logoimg = document.querySelector("img.logo");
+if(logoimg) logoimg.src = "https://raw.githubusercontent.com/Chrort/clarafilePlus/main/img/clarafile-logo-dark.jpg";
